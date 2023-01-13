852debae5f6f103407b27814c4691de4cfe917ca
uldiszz <zingisuldis@gmail.com>
2023-01-13 12:45:20 +0200

    Randomise row and animation duration for each message

--- SimpleChat/Views/Components/BulletChatView.swift
+++ SimpleChat/Views/Components/BulletChatView.swift
@@ -22,12 +22,13 @@ struct BulletMessage: Identifiable {
 struct BulletChatView: View {
     @EnvironmentObject var viewModel: ViewModel
     @State var bulletMessages: [BulletMessage] = []
+    @State var availableRows: [Int] = []
 
-    private let rowCount: CGFloat = 7
+    private let rowCount: Int = 7
     private let totalHeight = UIScreen.main.bounds.height - 150
     private let totalWidth = UIScreen.main.bounds.width
     private var rowHeight: CGFloat {
-        totalHeight / rowCount
+        totalHeight / CGFloat(rowCount)
     }
 
     var body: some View {
@@ -35,9 +36,9 @@ struct BulletChatView: View {
             ForEach(bulletMessages) { bulletMessage in
                 GeometryReader { geometry in
                     BulletMessageView(
-                        message: bulletMessage.message,
-                        startPosition: bulletMessage.position,
-                        size: geometry.size
+                        bulletMessages: $bulletMessages,
+                        bulletMessage: bulletMessage,
+                        animationTime: getRandomAnimationTime()
                     )
                 }
             }
@@ -45,57 +46,77 @@ struct BulletChatView: View {
         .frame(height: totalHeight)
         .onChange(of: viewModel.messages) { messages in
             if let message = messages.last as? Message {
-                let newBulletMessage = BulletMessage(message: message, position: GetPositionForNextBulletMessage())
+                let newBulletMessage = BulletMessage(message: message, position: getPositionForNextBulletMessage())
                 bulletMessages.append(newBulletMessage)
             }
         }
     }
 
-    private func GetPositionForNextBulletMessage() -> CGPoint {
-        var targetRow: CGFloat = 1
+    private func getPositionForNextBulletMessage() -> CGPoint {
+        if availableRows.isEmpty {
+            for row in 1...rowCount {
+                availableRows.append(row)
+            }
+        }
+        let targetRow = availableRows.randomElement() ?? 0
+        return CGPoint(x: totalWidth, y: CGFloat(targetRow) * rowHeight)
+    }
 
-        return CGPoint(x: totalWidth, y: targetRow * rowHeight)
+    private func getRandomAnimationTime() -> Double {
+        var time: Double = 6
+        time += Double.random(in: 0.5...1.5)
+        return time
     }
 }
 
 struct BulletMessageView: View {
-    var message: Message?
-    var startPosition: CGPoint = CGPoint.zero
-    var size: CGSize = CGSize.zero
+    @Binding var bulletMessages: [BulletMessage]
+    var bulletMessage: BulletMessage?
+    var animationTime: Double = 0
     @State private var xOffset: CGFloat = 0
 
-    init(message: Message, startPosition: CGPoint, size: CGSize) {
-        self.message = message
-        self.startPosition = startPosition
-        self.size = size
-        self.xOffset = UIScreen.main.bounds.width + size.width
+    init(bulletMessages: Binding<[BulletMessage]>, bulletMessage: BulletMessage, animationTime: Double) {
+        self._bulletMessages = bulletMessages
+        self.bulletMessage = bulletMessage
+        self.animationTime = animationTime
+        self.xOffset = UIScreen.main.bounds.width
     }
 
     var body: some View {
-        HStack {
-            switch message?.attributes?.type ?? .message {
-                case .message:
-                    Text(message?.content ?? "")
-                        .font(Constants.fAppLarge)
-                        .shadow(color: .black.opacity(0.75), radius: 4, x: 0, y: 2)
-                        .foregroundColor(.white)
-                        .padding(.vertical, 8)
-                case .sticker:
-                    if let stickerSrc = message?.attributes?.stickerSrc {
-                        RemoteImageView(imageURL: stickerSrc)
-                            .frame(width: 150, height: 150)
-                            .transition(.identity)
+        GeometryReader { proxy in
+            HStack {
+                switch bulletMessage?.message.attributes?.type ?? .message {
+                    case .message:
+                        Text(bulletMessage?.message.content ?? "")
+                            .font(Constants.fAppLarge)
+                            .shadow(color: .black.opacity(0.75), radius: 4, x: 0, y: 2)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 8)
+                            .lineLimit(1)
+                            .fixedSize()
+                    case .sticker:
+                        if let stickerSrc = bulletMessage?.message.attributes?.stickerSrc {
+                            RemoteImageView(imageURL: stickerSrc)
+                                .frame(width: 150, height: 150)
+                                .transition(.identity)
+                        }
+                }
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height)
+            .position(x: (bulletMessage?.position.x ?? 0) + proxy.size.width / 2, y: bulletMessage?.position.y ?? 0)
+            .onAppear {
+                withAnimation {
+                    xOffset = -((bulletMessage?.position.x ?? 0) + proxy.size.width) * 2
+                }
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + animationTime) {
+                    if let index = bulletMessages.firstIndex(where: { $0.id.uuidString == bulletMessage?.id.uuidString }) {
+                        bulletMessages.remove(at: index)
                     }
+                }
             }
+            .animation(.linear(duration: animationTime), value: xOffset)
+            .offset(x: xOffset + proxy.size.width)
         }
-        .frame(width: size.width, height: size.height)
-        .position(x: startPosition.x + size.width / 2, y: startPosition.y)
-        .onAppear {
-            withAnimation {
-                xOffset = -startPosition.x * 2
-            }
-        }
-        .animation(.linear(duration: 5), value: xOffset)
-        .offset(x: xOffset)
     }
 }
